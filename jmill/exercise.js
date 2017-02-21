dbc20bf210d28ec364ca1ffa575ccde3b54e631a
JMill <Jonathan@JMillville.com>
2017-02-21 10:38:29 -0500

    make more succinct

--- jmill/exercise.js
+++ jmill/exercise.js
@@ -9,10 +9,8 @@ exercise.getCourseList = function(courseName) {
     // enter code to list each person that takes this course
     for (var i = 0; i < dataLength; i++){
         person = people[i]
-        for (var j = 0; j < person.courses.length; j++){
-            if (person.courses[j] == '1.00'){
-                list.push(person);
-            }
+        if (person.courses.includes('1.00')){
+            list.push(person);
         }
     }
     return list;
